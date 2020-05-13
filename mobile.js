f802670d16efd1ffe76de1b15efaee155e60b53a
Jag Mort <ilya@kortkeros.com>
2020-05-13 20:38:57 +0300

    Increase delay

--- mobile.js
+++ mobile.js
@@ -12,5 +12,5 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > x.length) {slideIndex = 1}
   x[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
 }
