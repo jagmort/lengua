d843a45d0b8637e0efcd3ed8d7cf64e833a5fd89
ilya <jagmort@gmail.com>
2020-05-13 16:13:33 +0300

    auto carusel

--- mobile.js
+++ mobile.js
@@ -1,15 +1,16 @@
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("slide");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[slideIndex-1].style.display = "block";
-}
-  
 var slideIndex = 1;
 window.onload = function() {
-    showSlides(slideIndex);
+    showSlides();
+}
+
+function showSlides() {
+  var i;
+  var x = document.getElementsByClassName("slide");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
