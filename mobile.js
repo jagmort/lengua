c4b84108abc763ccb8ef4cb8b1ef76fb336328a7
ilya <jagmort@gmail.com>
2020-05-13 15:18:17 +0300

    clients' slider

--- mobile.js
+++ mobile.js
@@ -1,17 +1,21 @@
-var slideIndex = 0;
+var slideIndex = 1;
 window.onload = function() {
-    showSlides();
+    showSlides(slideIndex);
 }
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("slide");
+// Next/previous controls
+function plusSlides() {
+  showSlides(slideIndex += 1);
+}
 
-    console.log(slides.length);
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}    
-    slides[slideIndex-1].style.display = "block";  
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
 }
+
+document.getElementsByClassName("brands").addEventListener("click", plusSlides);
