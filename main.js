9d9b57fbad30bb7c094257623c8d917e7016f98b
jagmort <jagmort@gmail.com>
2020-05-13 12:20:30 +0300

    mobile detect

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
-if (screen.width <= 1280) {
-    document.location = "https://lengua.ai/mobile.html";
-}
+function detectmob() {
+    if(window.innerWidth <= 800 || window.innerHeight <= 600) {
+      return true;
+    } else {
+      return false;
+    }
+ }
+ 
+ if (detectmob()){
+    top.location.href="https://lengua.ai/mobile.html";
+ }
